533b11dcd33a8029bde5f973d422f68e27c09e62
Tobias Walter <tob-walter@gmx.de>
2025-08-01 18:08:18 +0200

    use vnc for josm

--- josm/docker/entrypoint.sh
+++ josm/docker/entrypoint.sh
@@ -3,14 +3,10 @@ set -e
 
 # Wait for web service API endpoint to be ready
 echo "⏳ Waiting for web API endpoint /api/capabilities on port 3000..."
-until curl -s --fail http://localhost:3000/api/capabilities > /dev/null; do
+until curl -s --fail http://web:3000/api/capabilities > /dev/null; do
   echo "🔄 Still waiting for /api/capabilities..."
   sleep 2
 done
 
-echo "✅ All services are up. Starting JOSM..."
-exec java \
-  --add-exports=java.base/sun.security.action=ALL-UNNAMED \
-  --add-exports=java.desktop/com.sun.imageio.plugins.jpeg=ALL-UNNAMED \
-  --add-exports=java.desktop/com.sun.imageio.spi=ALL-UNNAMED \
-  -jar /app/josm.jar
+echo "✅ All services are up. Starting Server. JOSM is ready..."
+exec /usr/bin/tini -- /dockerstartup/startup.sh
